f04c260d72ebac74c25d97228a543998b326a40a
unknown <ballmarsin2002@gmail.com>
2018-06-27 14:00:02 +0700

    typing indicator

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const BootBot = require('bootbot');
+const BootBot = require('./Components/bootbot');
 const config = require('./config/configProd');
 require('isomorphic-fetch');
 const bot = new BootBot({
